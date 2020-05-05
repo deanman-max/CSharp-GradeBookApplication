1355c08693166b66c3c7fdd7b5b375386a567871
deanman-max <60674705+deanman-max@users.noreply.github.com>
2020-05-05 18:39:56 -0500

    Mod 1

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,18 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Name = name;
+            Type = GradebookType.Ranked;
+
+        }
+
+    }
+}
