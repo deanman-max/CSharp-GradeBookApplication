d0ef47fe125e3da087f2f18b6f920d884d0150ee
deanman-max <60674705+deanman-max@users.noreply.github.com>
2020-05-05 19:44:10 -0500

    mod 2

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -13,6 +13,22 @@ namespace GradeBook.GradeBooks
             Type = GradeBookType.Ranked;
 
         }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new ArgumentException("Ranked-grading requires a minimum of 5 students to work.");
+
+            if (averageGrade >= 90)
+                return 'A';
+            else if (averageGrade >= 80)
+                return 'B';
+            else if (averageGrade >= 70)
+                return 'C';
+            else if (averageGrade >= 60)
+                return 'D';
+            else
+                return 'F';
+        }
 
     }
 }
