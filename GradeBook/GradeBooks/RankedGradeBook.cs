3a814447917ce39d1e60a7ccfaf8c8f8cc1fae80
deanman-max <60674705+deanman-max@users.noreply.github.com>
2020-05-05 20:06:08 -0500

    corrections

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -35,14 +35,14 @@ namespace GradeBook.GradeBooks
         public override void CalculateStatistics()
         {
             if (Students.Count < 5)
-                throw new ArgumentException("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
             base.CalculateStatistics();
 
         }
         public override void CalculateStudentStatistics(string name)
         {
             if (Students.Count < 5)
-                throw new ArgumentException("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
             base.CalculateStudentStatistics(name);
 
         }
